907ed4df224688b208110f92fdf09dbc7478872c
Hai Nam <nam@swlog.com.sg>
2020-06-13 21:52:20 +0700

    -Update setting.

--- main/main/settings.py
+++ main/main/settings.py
@@ -39,12 +39,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'main.urls'
 
-WEB_DIR = os.path.join(BASE_DIR, 'static')
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [WEB_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -91,11 +89,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/web/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'MobileWallet2020.Customer'
 
